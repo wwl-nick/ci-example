e240966521d0bef4fe6b7b83244af09dad2db2c4
wwl-nick <131856519+wwl-nick@users.noreply.github.com>
2024-01-10 12:28:36 +0100

    edited: ALLOWED_HOSTS

--- ci_example/settings.py
+++ ci_example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b9ge74_fq@&v025!v028^90bartkk^0k+&8bh=7vxl1-9qz5)*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
