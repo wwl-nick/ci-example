cd948ff0ffc2b509d728fc4cef6fd3f4795e33df
wwl-nick <131856519+wwl-nick@users.noreply.github.com>
2024-01-10 10:16:40 +0100

    initial

---
+++ ci_example/example/tests.py
@@ -0,0 +1,19 @@
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexURLTestCase(TestCase):
+    def test_index_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+class IndexViewTestCase(TestCase):
+    def test_index_view(self):
+        url = reverse('index')
+
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertEqual(response.context['text'], 'Hello World!')
---
+++ ci_example/example/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def index(request):
+    text = 'Hello World!'
+    return render(request, 'index.html', {'text': text})
